d7cf36ee1a7e942da870b4ac15152d135f62e4cf
Ilan <53833154+IlanLuci@users.noreply.github.com>
2026-05-31 21:44:09 +0200

    performance improvements

--- utils/db.js
+++ utils/db.js
@@ -5,7 +5,12 @@ const db = mysql.createPool({
     port: process.env.PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    waitForConnections: true,
+    connectionLimit: 15,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 10000,
 });
 
 module.exports = db;
